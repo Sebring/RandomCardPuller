d0260afb1d205520efd9069fca41a04f1f0ba823
Johan Sebring <johan.sebring@gmail.com>
2016-04-08 11:50:15 +0200

    Add images

--- calculator.js
+++ calculator.js
@@ -4,6 +4,21 @@ var def_box = {cards:[
 	{population:8, drawn:0, type:"epic"}, {population:8, drawn:0, type:"epic"}, {population:8, drawn:0, type:"epic"}, {population:8, drawn:0, type:"epic"}, {population:8, drawn:0, type:"epic"}, {population:8, drawn:0, type:"epic"}	
 ]};
 
+var _box_pic = [
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/High_Knight_Frog_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Ethereal_Angel_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/2b18a9af0fcb5fc15af04b7dd663958caaf75116/res/cardImages/Goblin_Explosioneer_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Legendary_Chaos_Frog_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/f53fea9f9180f29104ac24267e7ac1733ba5b30f/res/cardImages/Giant_Sword_Frog_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Rhino_Beast_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/f53fea9f9180f29104ac24267e7ac1733ba5b30f/res/cardImages/Cloud_Pike_Frog_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Concealed_Dragon_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Spike_Frog_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Spinehead_Undead_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Frog_Shaman_B.jpg",
+	"https://cdn.rawgit.com/TheSench/SIMSpellstone/gh-pages/res/cardImages/Huntress_Frog_B.jpg"];
+
+
 var _box = def_box;
 var _tot_shards = 0;
 var _tot_pulls = 0;
@@ -19,6 +34,14 @@ $(document).ready(function() {
 	$('#btnSpend').click(function() { spendAll()});
 
 });
+
+function addImages() {
+	$('.card > div').each( function(index) {
+		var $this = $(this);
+		console.log(_box_pic[index]);
+		$this.css('background-image', 'url(' + _box_pic[index] + ')');
+	});
+}
 
 function calculateNumPulls() {
 	var shards = $('#num_shards').val();
@@ -76,11 +99,12 @@ function updateForm() {
 }
 
 function updateBox() {
-	var template = doT.template('{{~it.cards :value:index}}<div class="col-xs-2 col-sm-1 card"><div class="{{=value.type}}"><div class="pop text-center"><span class="d" data-drawn="{{=value.drawn}}">{{=value.drawn}}</span>/<span class="p" data-pop="{{=value.population}}">{{=value.population}}</span></div></div></div>{{~}}');
+	var template = doT.template('{{~it.cards :value:index}}<div class="col-xs-2 col-sm-1 card"><div class="{{=value.type}}"><div class="pop text-center badge"><span class="d" data-drawn="{{=value.drawn}}">{{=value.drawn}}</span>/<span class="p" data-pop="{{=value.population}}">{{=value.population}}</span></div></div></div>{{~}}');
 	var data = template(_box);
 	$('#box_sim').html(data);
 
 	checkDualsAndQuads();
+	addImages();
 
 	return data;
 }
