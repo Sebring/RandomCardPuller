0487a20291fdf61230ed0674c55452eb3446f8f9
Johan Sebring <johan.sebring@sogeti.se>
2016-04-21 19:34:57 +0200

    Add Devourer box

--- box_data.js
+++ box_data.js
@@ -1,18 +1,18 @@
 var box_pic = false;
 var chance_pic = false;
 box_pic=
-[ { id: '5014', pic: 'Raptor_Bird_A' },
-  { id: '5023', pic: 'Night_Dragon_A' },
-  { id: '6026', pic: 'Smoke_Dragon_A' },
-  { id: '6052', pic: 'Fuming_Horror_A' },
-  { id: '7046', pic: 'Root_Elemental_A' },
-  { id: '7018', pic: 'Emperor_Frog_A' },
-  { id: '5050', pic: 'Cloud_Pike_Frog_A' },
-  { id: '5053', pic: 'Frog_Mage_A' },
-  { id: '6004', pic: 'Branding_Metal_Construct_A' },
-  { id: '6020', pic: 'Spike_Frog_A' },
-  { id: '7051', pic: 'Big_And_Little_A' },
-  { id: '7005', pic: 'Water_Manipulator_A' } ]
+[ { id: '5052', pic: 'Paradise_Bird_A' },
+  { id: '5018', pic: 'High_Knight_Frog_A' },
+  { id: '6047', pic: 'Death_Elemental_A' },
+  { id: '6040', pic: 'Fire_Sword_Angel_A' },
+  { id: '7052', pic: 'Frog_Devourer_A' },
+  { id: '7042', pic: 'Frog_Pirates_A' },
+  { id: '5020', pic: 'Frozen_Frog_A' },
+  { id: '5030', pic: 'Mastodon_Mage_A' },
+  { id: '6051', pic: 'Camouflaged_Assassin_A' },
+  { id: '6011', pic: 'Bomb_Spirit_A' },
+  { id: '7020', pic: 'Huntress_Frog_A' },
+  { id: '7035', pic: 'Undead_Tiger_A' } ]
 ;
 chance_pic=
 [ { id: '7049', pic: 'Steam_Roller_A' },
